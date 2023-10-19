a25008b71ab46f6892a68e68513e4ab9372ee6f9
matpl124 <matteocerqua69@gmail.com>
2023-10-19 17:51:52 +0200

    creation_form_ficheFraie

--- vue/pageVisiteur.php
+++ vue/pageVisiteur.php
@@ -12,6 +12,12 @@
         <h2><?php echo $title; ?></h2>
         <p>Visiteurs</p>
     </main>
+    <form action="../controller/horsForfait.php">
+        <h2>Hors forfait</h2>
+        <input type="text" name ="libelle" placeholder ="libelle" required>
+        <input type="number" name ="montant" placeholder ="montant" required>
+        <input type="submit" name ="submit" placeholder ="valider">
+    </form>
 </body>
 <script src="../dist/viewDashboard.js"></script>
 </html>
