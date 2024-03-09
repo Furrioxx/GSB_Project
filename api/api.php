2a3059b9b9d4eb3acfa21314fa426fb58456bef6
Furrioxx <pelud.tom1@gmail.com>
2024-03-09 17:01:46 +0100

    adding route process for api

---
+++ api/api.php
@@ -0,0 +1,115 @@
+<?php
+
+
+class Api{
+
+    private $db;
+
+    public function __construct($pdo)
+    {
+        $this->db = $pdo;
+    }
+
+    public function login(){
+        header('Content-Type: application/json');
+        include_once('token.php');
+        $mail = $_POST['mail'];
+        $pass = $_POST['password'];
+    
+        $fucntionSQL = new request();
+        $result = $fucntionSQL->getUser($this->db, $mail);
+    
+        if(!empty($result)){
+            foreach ($result as $key => $value) {
+                if(strlen($pass) > 8){
+                    if(password_verify($pass, $value['password'])){
+                        $json = array('status' => 200, 'message' => 'connecté', 'token' => generateToken($mail, $this->db));
+                        foreach ($result as $key => $value) {
+                            $json[$key] = $value;
+                        }
+                    }
+                    else{
+                        $json = array('status' => 400, 'message' => 'mauvais password');
+                    }
+                }
+            }
+        }
+        else{
+            $json = array('status' => 400, 'message' => 'mauvais mail');
+        }
+    
+        echo json_encode($json);
+    }
+
+    public function getCostSheet(){
+        include('token.php');
+
+        header('Content-Type: application/json');
+
+        $idUser = $_POST['id'];
+        $isTraite = $_POST['isTraite'];
+        $token = $_POST['token'];
+        $mail = $_POST['mail'];
+
+        if(verifyToken($token, $mail, $db)){
+            $fucntionSQL = new request();
+            $result = $fucntionSQL->getCostSheet($db, $idUser);
+            $json = array('status' => 200, 'function' => 'get cost sheet');
+            foreach ($result as $key => $value) {
+                if($isTraite == "T"){
+                    if($value['statue'] == $isTraite){
+                        $json['data'][$key] = $value;
+                    }
+                    else{
+                        continue;
+                    }
+                    
+                }
+                else if($isTraite == "NT"){
+                    if($value['statue'] == $isTraite){
+                        $json['data'][$key] = $value;
+                    }
+                    else{
+                        continue;
+                    }
+                }
+                else if($isTraite == "all"){
+                    $json['data'][$key] = $value;
+                }
+                
+            }  
+
+            echo json_encode($json); 
+        }
+        else{
+            $json = array('status' => 400, 'message' => 'token invalide ou expiré veuillez vous reconnecter', 'mail' => $mail, 'id' => $idUser, "token" => $token, 'isTraite' => $isTraite);
+            echo json_encode($json); 
+        }
+    }
+
+    public function getCost(){
+        include('token.php');
+
+        header('Content-Type: application/json');
+
+        $idFicheFrais = $_POST['idFicheFrais'];
+        $token = $_POST['token'];
+        $mail = $_POST['mail'];
+
+        if(verifyToken($token, $mail, $db)){
+                $fucntionSQL = new request();
+                $result = $fucntionSQL->getAllCost($db, $idFicheFrais);
+                $json = array('status' => 200, 'statut' => 'Succès');
+                foreach ($result as $key => $value) {
+                        $json['data'][$key] = $value;
+                }
+
+                echo json_encode($json); 
+        }
+        else{
+            $json = array('status' => 400, 'message' => 'token invalide ou expiré veuillez vous reconnecter', 'mail' => $mail, 'id' => $idUser, "token" => $token, 'isTraite' => $isTraite);
+            echo json_encode($json); 
+        }
+        
+    }
+}
---
+++ api/dispatcher.php
@@ -0,0 +1,50 @@
+<?php
+
+include_once "api.php";
+include('../model/functionSQL.php');
+
+
+try {
+    if (!empty($_GET['route'])) {
+    $url = explode("/", filter_var($_GET['route'], FILTER_SANITIZE_URL));
+    // print_r($url);
+    // Filtré l'URL et ajoute de la sécurité.
+    switch ($url[0]) {
+        case "login":
+            if (empty($url[1])) {
+                $api = new Api($db);
+                $api->login();
+            }
+        break;
+        case "getCostSheet":
+            if (empty($url[1])) {
+                $api = new Api($db);
+                $api->getCostSheet();
+            }
+            break;
+        case "getCost":
+            if (empty($url[1])) {
+                $api = new Api($db);
+                $api->getCost();
+            }
+            break;
+        // case "article":
+        //     if (!empty($url[1])) {
+        //         getArticleById($url[1]);
+        //     } else {
+        //         throw new Exception("Vous n'avez pas renseigné le numéro de l'article");
+        //     }
+        //     break;
+        default:
+            throw new Exception("La demande n'est pas valide, vérifiez l'url");
+    }
+} else {
+    throw new Exception("Problème de récupération de données");
+}
+} catch (Exception $e) {
+    $erreur = [
+        "message" => $e->getMessage(),
+        "code" => $e->getCode()
+    ];
+    print_r($erreur);
+}
